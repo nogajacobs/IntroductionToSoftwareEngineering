60901df63765eae9e02decc33412f2bca9672aff
noa-m <noa9973@gmail.com>
2022-03-08 16:22:35 +0200

    temp>0,v1.dotProduct(u),"ERROR: the normalized vector is opposite to the original one

--- unitTests/primitives/VectorTest.java
+++ unitTests/primitives/VectorTest.java
@@ -24,19 +24,26 @@ class VectorTest {
 
     @Test
     void testTestAdd() {
+        assertEquals(new Vector(-1, -2, -3),
+                v1.add(v2),
+                "Wrong vector add");
     }
 
     @Test
     void testScale() {
+        assertEquals(new Vector(2, 4, 6),
+                v1.Scale(2.0),
+                "Wrong vector scale");
     }
 
     /**
      * מקרה קצה של 0
      */
     @Test
-    void testTestDotProductBV() {
+    void testTestDotProduct() {
 
         assertEquals(0,v1.dotProduct(v3),"ERROR: dotProduct() for orthogonal vectors is not zero");
+        assertEquals(-28,v1.dotProduct(v2),"ERROR: dotProduct() wrong value");//מקרה פשוט
 
     }
 
@@ -45,13 +52,34 @@ class VectorTest {
      */
     @Test
     void testTestCrossProductDP() {
-        assertEquals(-28,v1.dotProduct(v2),"ERROR: dotProduct() wrong value");
-
+        assertThrows(//test zero vector
+                IllegalArgumentException.class,
+                ()->v1.crossProduct(v2),
+                "ERROR: crossProduct() for parallel vectors does not throw an exception"
+        );
+        Vector vr = v1.crossProduct(v3);
+        assertEquals(v1.length() * v3.length(),vr.length(),0.01,"ERROR: crossProduct() wrong result length");
+        assertEquals(0,vr.dotProduct(v1),0.01,"ERROR: crossProduct() result is not orthogonal to its operands");
+        assertEquals(0,vr.dotProduct(v3),0.01,"ERROR: crossProduct() result is not orthogonal to its operands");
     }
+
+
 
     @Test
     void testTestNormalize() {
+        Vector u = v1.normalize();
+        double temp=v1.dotProduct(u);
+        assertEquals(1,u.length() ,"ERROR: the normalized vector is not a unit vector");
+        assertThrows(//test zero vector
+                IllegalArgumentException.class,
+                ()->v1.crossProduct(u),
+                "ERROR: the normalized vector is not parallel to the original one"
+        );
+        assertTrue(temp>0,"ERROR: the normalized vector is opposite to the original one");
+        //  assertEquals(temp>0,v1.dotProduct(u),"ERROR: the normalized vector is opposite to the original one");
+
     }
+
 
     @Test
     void testTestLength() {
