8e97d351ee44ef66df423b128d58c4f85c3505a0
noga oren <orog1234@gmail.com>
2022-03-08 11:18:17 +0200

    add testVector

---
+++ unitTests/primitives/VectorTest.java
@@ -0,0 +1,36 @@
+package primitives;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VectorTest {
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void scale() {
+    }
+
+    @Test
+    void dotProduct() {
+    }
+
+    @Test
+    void crossProduct() {
+    }
+
+    @Test
+    void normalize() {
+    }
+
+    @Test
+    void length() {
+    }
+
+    @Test
+    void lengthSquared() {
+    }
+}
