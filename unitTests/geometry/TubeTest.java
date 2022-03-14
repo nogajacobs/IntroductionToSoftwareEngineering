614fb3d575ad5b9a19947fab2f5a6b2124f43af5
noga oren <orog1234@gmail.com>
2022-03-14 23:08:05 +0200

    add test getNormal in testCylinder finish

--- unitTests/geometry/TubeTest.java
+++ unitTests/geometry/TubeTest.java
@@ -12,22 +12,11 @@ class TubeTest {
     @Test
     void testGetNormal() {
         Point P0 = new Point(0,0,3);
-        Point P = new Point(1,0,0);
-        double r=1;
-        Vector vector = new Vector(0,0,-1);
-        Tube tube = new Tube(new Ray(P0,vector),r);
-        assertEquals(new Vector(1,0,0),tube.getNormal(P),"ERROR  ");
-
-
-
-
-        Point P0 = new Point(0,0,3);
         Point P1 = new Point(1,0,0);
         double r=1;
         Vector vector = new Vector(0,0,-1);
         Tube tube = new Tube(new Ray(P0,vector),r);
         assertEquals(new Vector(1,0,0),tube.getNormal(P1),"ERROR  ");
-
 
         Point P2 = new Point(1,0,2);
         assertEquals(new Vector(1,0,0),tube.getNormal(P2),"ERROR   ");
